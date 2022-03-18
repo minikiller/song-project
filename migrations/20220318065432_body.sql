5f0967a54c4c7c718c43c9a305ad4e10cd09c881
Sun Lingfeng, Rise Sun <sunlingfeng70@gmail.com>
2022-03-18 14:54:56 +0800

    add it

---
+++ migrations/20220318065432_body.sql
@@ -0,0 +1,21 @@
+-- Add migration script here
+-- Add migration script here
+drop table if exists body;
+
+create table body 
+(
+    id serial primary key,
+    device_id INT not null,
+    content varchar(1024) not null,
+    time TIMESTAMP default now()
+);
+
+insert into body
+(id,device_id,content,time)
+values
+(1,1,'First course','2022-01-17 05:40:00');
+
+insert into body
+(id,device_id,content,time)
+values
+(2,1,'Second course','2022-01-17 05:40:00');
